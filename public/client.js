898ea906db114b9580729b387e6f82f7d8c6ceb5
Marc Pomar <marc@faable.com>
2016-10-11 15:40:43 +0200

    Add Jquery and CSS3 animations

--- public/client.js
+++ public/client.js
@@ -2,5 +2,13 @@
  * Created by boyander on 11/10/16.
  */
 $(document).ready(function(){
-    //alert("This is Hello World by JQuery");
+
+    $('.img-rounded').mouseenter(function() {
+        $(this).animate({zoom:'1.1'},'fast');
+        $(this).parent().addClass("selected");
+    });
+    $('.img-rounded').mouseout(function() {
+        $(this).animate({zoom:'1'},'fast');
+        $(this).parent().removeClass("selected");
+    });
 });
